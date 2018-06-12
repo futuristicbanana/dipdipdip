9be8a6f9cf310b692cd685ab612c8e1dc949ff5f
futuristicbanana <kaudreyitching@gmail.com>
2018-06-11 22:24:08 -0700

    Add all files

--- local_settings.py
+++ local_settings.py
@@ -19,12 +19,12 @@ USER_ACCESS_FILENAME = '' # The MASTODON user credential file you created at ins
 
 # Sources (Twitter, Mastodon, local text file or a web page)
 SOURCE_ACCOUNTS = ["AudreyKitching"]  # A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
-TWITTER_SOURCE_ACCOUNTS = [""]  # A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
+TWITTER_SOURCE_ACCOUNTS = ["AudreyKitching"]  # A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
 MASTODON_SOURCE_ACCOUNTS = [""] # A list, e.g. ["@user@instance.tld"]
 SOURCE_EXCLUDE = r'^$'  # Source tweets that match this regexp will not be added to the Markov chain. You might want to filter out inappropriate words for example.
-STATIC_TEST = False  # Set this to True if you want to test Markov generation from a static file instead of the API.
+STATIC_TEST = True  # Set this to True if you want to test Markov generation from a static file instead of the API.
 TEST_SOURCE = ".txt"  # The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API. You can use the included testcorpus.txt, if needed.
-SCRAPE_URL = False  # Set this to true to scrape a webpage.
+SCRAPE_URL = True  # Set this to true to scrape a webpage.
 SRC_URL = ['http://www.example.com/one', 'https://www.example.com/two']  # A comma-separated list of URLs to scrape
 WEB_CONTEXT = ['span', 'h2']  # A comma-separated list of the tag or object to search for in each page above.
 WEB_ATTRIBUTES = [{'class': 'example-text'}, {}] # A list of dictionaries containing the attributes for each page.
@@ -33,4 +33,4 @@ ODDS = 1  # How often do you want this to run? 1/8 times?
 ORDER = 2  # How closely do you want this to hew to sensical? 2 is low and 4 is high.
 
 DEBUG = False  # Set this to False to start Tweeting live
-TWEET_ACCOUNT = "kaudreyitching"  # The name of the account you're tweeting to.
+TWEET_ACCOUNT = "KaudreyItching"  # The name of the account you're tweeting to.
